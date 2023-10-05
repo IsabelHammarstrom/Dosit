d673a89208a83500d33e7d56f542ba4cb6eefac7
ati5moradi <142426403+ati5moradi@users.noreply.github.com>
2023-10-05 16:38:56 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,178 +1,156 @@
-/* #include "gd32vf103.h"
-#include "drivers.h"
-#include "lcd.h"
+/*!
+    \file  main.c
+    \brief RTC calendar 
 
-int main(void){
-    int ms=0, s=0, key, pKey=-1, c=0, idle=0;
-    int lookUpTbl[16]={1,4,7,14,2,5,8,0,3,6,9,15,10,11,12,13};
-    int dac=0, speed=-100;
-    int adcr, tmpr;
-    char digits[10][10]={"Zero ","One  ","Two  ","Three","Four ","Five ","Six  ","Seven","Eight","Nine "};
+    \version 2019-6-5, V1.0.0, firmware for GD32VF103
+*/
 
-    t5omsi();                               // Initialize timer5 1kHz
-    colinit();                              // Initialize column toolbox
-    l88init();                              // Initialize 8*8 led toolbox
-    keyinit();                              // Initialize keyboard toolbox
-    Lcd_SetType(LCD_NORMAL);                // or use LCD_INVERTED!
-    Lcd_Init();
-    LCD_Clear(RED);
-    LCD_ShowStr(10, 10, "POLL VERSION", WHITE, TRANSPARENT);
+/*
+    Copyright (c) 2019, GigaDevice Semiconductor Inc.
 
-    while (1) {
-        idle++;                             // Manage Async events
-        LCD_WR_Queue();                   // Manage LCD com queue!
+    Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
 
-        if (t5expq()) {                     // Manage periodic tasks
-            l88row(colset());               // ...8*8LED and Keyboard
-            ms++;                           // ...One second heart beat
-            if (ms==1000){
-              ms=0;
-              l88mem(0,s++);
-              LCD_ShowStr(10, 30, digits[s%10], WHITE, OPAQUE);
-            }
-            if ((key=keyscan())>=0) {       // ...Any key pressed?
-              if (pKey==key) c++; else {c=0; pKey=key;}
-              l88mem(1,lookUpTbl[key]+(c<<4));
-            }
-            l88mem(2,idle>>8);              // ...Performance monitor
-            l88mem(3,idle); idle=0;
-        }
-    }
-} */
+    1. Redistributions of source code must retain the above copyright notice, this 
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, 
+       this list of conditions and the following disclaimer in the documentation 
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors 
+       may be used to endorse or promote products derived from this software without 
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+OF SUCH DAMAGE.
+*/
 
 #include "gd32vf103.h"
-#include "drivers.h"
-#include "lcd.h"
+#include "gd32vf103v_eval.h"
+#include "rtc.h"
+#include <stdio.h>
 
+#define BKP_DATA_REG_NUM              42
 
-int main(void){
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    /* COM0 configure */
+    gd_eval_com_init(EVAL_COM0);
 
-    int ms=0, s=0, m=0, h=0, dayCounter=0, key, pKey=-1, c=0, idle=0;
-    int lookUpTbl[16]={1,4,7,14,2,5,8,0,3,6,9,15,10,11,12,13};
-    int dac=0, speed=-100;
-    int check=0, counter=0, tmp, theTime;
+    /* ECLIC configure */
+    eclic_global_interrupt_enable();
+    eclic_set_nlbits(ECLIC_GROUP_LEVEL3_PRIO1);
+    eclic_irq_enable(RTC_IRQn, 1, 0);
 
-    t5omsi();                               // Initialize timer5 1kHz
-    colinit();                              // Initialize column toolbox
-    l88init();                              // Initialize 8*8 led toolbox
-    keyinit();                              // Initialize keyboard toolbox
-    Lcd_SetType(LCD_NORMAL);                // or use LCD_INVERTED!
+    printf( "\r\n This is a RTC demo...... \r\n" );
 
-    Lcd_Init();
-    LCD_Clear(BLACK);
+    if (bkp_data_read(BKP_DATA_0) != 0xA5A5){
+        /* backup data register value is not correct or not yet programmed
+        (when the first time the program is executed) */
+        printf("\r\nThis is a RTC demo!\r\n");
+        printf("\r\n\n RTC not yet configured....");
 
-    //LCD_ShowChar(10,60,126,0,YELLOW);    // ENLIGT storleken //ny  // bitar mönster står i oledfont.h
+        /* RTC configuration */
+        rtc_configuration();
 
+        printf("\r\n RTC configured....");
 
-    while (1) {
-        idle++;                             // Manage Async events
-        LCD_WR_Queue();                   // Manage LCD com queue!
+        /* adjust time by values entred by the user on the hyperterminal */
+        time_adjust();
 
-        if (t5expq()) {                     // Manage periodic tasks
-          l88row(colset());               // ...8*8LED and Keyboard
+        bkp_data_write(BKP_DATA_0, 0xA5A5);
+    }else{
+        /* check if the power on reset flag is set */
+        if (rcu_flag_get(RCU_FLAG_PORRST) != RESET){
+            printf("\r\n\n Power On Reset occurred....");
+        }else if (rcu_flag_get(RCU_FLAG_SWRST) != RESET){
+            /* check if the pin reset flag is set */
+            printf("\r\n\n External Reset occurred....");
+        }
+        
+        /* allow access to BKP domain */
+        rcu_periph_clock_enable(RCU_PMU);
+        pmu_backup_write_enable();
+        
+        printf("\r\n No need to configure RTC....");
+        /* wait for RTC registers synchronization */
+        rtc_register_sync_wait();
 
-          key=keyscan();
-          switch (key){                                                               //knappar     //ny
-            case 15:{ 
-              LCD_Clear_Days(BLACK);                                                            // knapp 1
-              LCD_ShowStr(10, 10, "MON", RED, TRANSPARENT); break;
-              }      
-            case 14:{
-              LCD_Clear_Days(BLACK);                                                             // knapp 2
-              LCD_ShowStr(10, 10, "TUS", RED, TRANSPARENT); break;
-              }      
-            case 13:{
-              LCD_Clear_Days(BLACK);                                                               // knapp 3
-              LCD_ShowStr(10, 10, "WED", RED, TRANSPARENT); break;
-              }      
-            case 11:{
-              LCD_Clear_Days(BLACK);                                                              // knapp 4
-              LCD_ShowStr(10, 10, "THU", RED, TRANSPARENT); break;
-              }      
-            case 10:{
-              LCD_Clear_Days(BLACK);                                                              // knapp 5
-              LCD_ShowStr(10, 10, "FRI", RED, TRANSPARENT); break;
-              }      
-            case 9:{
-              LCD_Clear_Days(BLACK);                                                                // knapp 6
-              LCD_ShowStr(10, 10, "SAT", RED, TRANSPARENT); break;
-              }      
-            case 7:{
-              LCD_Clear_Days(BLACK);                                                                 // knapp 7
-              LCD_ShowStr(10, 10, "SUN", RED, TRANSPARENT); break;
-              }       
-          }  
+        /* enable the RTC second */
+        rtc_interrupt_enable(RTC_INT_SECOND);
 
-          ms++;                           // ...One second heart beat
-          if (ms==1000){
-            ms=0;
-            l88mem(0,s++);
-            if (s == 60) {
-              s = 0;
-              m++;
-              if (m>0){
-                LCD_ShowChar(30 + 45, 38, ':', 0, WHITE);
-              }
-              LCD_ShowNum(20 + 65, 40, m, 2, WHITE);
-              if (m == 60) {
-                m = 0;
-                h++;
-                LCD_ShowNum(50, 40, h, 2, WHITE);
-                if (h == 23) {
-                  dayCounter++;
-                  h = 0;
-                }
-              }
+        /* wait until last write operation on RTC registers has finished */
+        rtc_lwoff_wait();
+    }
+
+#ifdef RTCCLOCKOUTPUT_ENABLE
+    /* enable PMU and BKPI clocks */
+    rcu_periph_clock_enable(RCU_BKPI);
+    rcu_periph_clock_enable(RCU_PMU);
+    /* allow access to BKP domain */
+    pmu_backup_write_enable();
+
+    /* disable the tamper pin */
+    bkp_tamper_detection_disable();
+
+    /* enable RTC clock output on tamper Pin */
+    bkp_rtc_calibration_output_enable();
+#endif
+
+    /* clear reset flags */
+    rcu_all_reset_flag_clear();
+
+    /* display time in infinite loop */
+    time_show();
+
+    while (1){
+    }
+}
+
+/*!
+    \brief      check if the backup data registers are clear or not
+    \param[in]  none
+    \param[out] none
+    \retval     the number of data register
+*/
+uint32_t is_backup_register_clear(void)
+{
+    uint32_t temp = 0;
+
+    for(temp = 0; temp < BKP_DATA_REG_NUM; temp++){
+        if(temp < 10){
+            /* check if the data of data register 0-9 is 0x0000 */
+            if(0x0000 != BKP_DATA_GET(BKP_DATA0_9(temp))){
+                return temp+1;
             }
-          }
-            
-            
-
-
-          if ((key=keyscan())) {       // ...Any key pressed?
-              if ((key==12) ||(key==8) ||(key==4)){
-                if (check==0){
-                  check=1;
-                  tmp=key;
-                }  
-              }
-              if (key==1){
-                if (check==1){
-                  check=0;
-                  LCD_ShowStr(20, 60, "TEST", RED, TRANSPARENT);
-                }
-              }
-          }
-
-            
-            
-            switch (dayCounter) {
-            case 1:{LCD_Clear_Days(BLACK); LCD_ShowStr(10, 40, "MON", WHITE, TRANSPARENT);break;}
-            case 2:{LCD_ShowStr(10, 40, "TUE", WHITE, TRANSPARENT);break;}
-            case 3:{LCD_ShowStr(10, 40, "WED", WHITE, TRANSPARENT);break;}
-            case 4:{LCD_ShowStr(10, 40, "THU", WHITE, TRANSPARENT);break;}
-            case 5:{LCD_ShowStr(10, 40, "FRI", WHITE, TRANSPARENT);break;}
-            case 6:{LCD_ShowStr(10, 40, "SAT", WHITE, TRANSPARENT);break;}
-            case 7:{LCD_ShowStr(10, 40, "SUN", WHITE, TRANSPARENT);break;}
-            default:
-              break;
+        }else{
+            /* check if the data of data register 10-41 is 0x0000 */
+            if(0x0000 != BKP_DATA_GET(BKP_DATA10_41(temp))){
+                return temp+1;
             }
-            
-            
-
-
-    
-              
-              
-              
-
-              //if (pKey==key) c++; else {c=0; pKey=key;}
-
-              l88mem(1,lookUpTbl[key]+(c<<4));
-            
-            
-            l88mem(2,idle>>8);              // ...Performance monitor
-            l88mem(3,idle); idle=0;
         }
     }
+    return 0;
+}
+
+/* retarget the C library printf function to the USART */
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(EVAL_COM0, (uint8_t)ch);
+    while(RESET == usart_flag_get(EVAL_COM0, USART_FLAG_TBE));
+
+    return ch;
 }
